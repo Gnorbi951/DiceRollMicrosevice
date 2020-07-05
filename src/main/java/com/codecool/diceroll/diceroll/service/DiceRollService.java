beee109f0eb18d7f179909fc9d65c4faece56fc6
Gőcze Norbert <gnorbi951@gmail.com>
2020-07-05 20:07:54 +0200

    Add RollD6 and test

--- src/main/java/com/codecool/diceroll/diceroll/service/DiceRollService.java
+++ src/main/java/com/codecool/diceroll/diceroll/service/DiceRollService.java
@@ -1,13 +1,20 @@
 package com.codecool.diceroll.diceroll.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
 
 @Service
+@Slf4j
 public class DiceRollService {
 
     @Autowired
     private Random random;
+
+    public int rollD6() {
+        log.info("Roll D6");
+        return random.nextInt(6)+1;
+    }
 }
--- src/test/java/com/codecool/diceroll/diceroll/service/DiceRollServiceTest.java
+++ src/test/java/com/codecool/diceroll/diceroll/service/DiceRollServiceTest.java
@@ -1,13 +1,26 @@
 package com.codecool.diceroll.diceroll.service;
 
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class DiceRollServiceTest {
 
+    @Autowired
+    private DiceRollService service;
+
+    @RepeatedTest(50)
+    public void rollD6() {
+        int value = service.rollD6();
+        // Cant find isBetween method
+        assertTrue(value > 0);
+        assertTrue(value < 7);
+    }
 }
