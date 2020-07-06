fb4f41801ff4706acc171867e77b777329501ced
Gőcze Norbert <gnorbi951@gmail.com>
2020-07-06 13:56:26 +0200

    Figure out swagger-ui dependencies and annotations

--- src/main/java/com/codecool/diceroll/diceroll/DicerollApplication.java
+++ src/main/java/com/codecool/diceroll/diceroll/DicerollApplication.java
@@ -3,10 +3,19 @@ package com.codecool.diceroll.diceroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.time.LocalDate;
 import java.util.Random;
 
 @SpringBootApplication
+@EnableSwagger2
 public class DicerollApplication {
 
 	public static void main(String[] args) {
@@ -15,5 +24,17 @@ public class DicerollApplication {
 	@Bean
 	public Random random() {
 		return new Random();
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.ant("/roll/**"))
+				.build();
+//				.pathMapping("/api")
+//				.directModelSubstitute(LocalDate.class, String.class)
+//				.genericModelSubstitutes(ResponseEntity.class);
 	}
 }
